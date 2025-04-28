289a868a8559d2917143130482aa7b89d9222de1
Fanshi13 <aquillafazanaufan@gmail.com>
2025-04-28 11:09:43 +0700

    commit 1

--- class2.cpp
+++ class2.cpp
@@ -7,29 +7,4 @@ private:
     int panjang;
     int lebar;
     int luas;
-
-public:
-    void input_data()
-    {
-        cout << "Masukkan panjang = ";
-        cin >> panjang;
-        cout << "Masukkan lebar = ";
-        cin >> lebar;
-    }
-    void output_data()
-    {
-        cout << " Tampilkan luas = " << luas << endl;
-    }
-    void hitung_luas()
-    {
-        luas = panjang * lebar;
-    }
-};
-
-int main()
-{
-    persegi_panjang pp;
-    pp.input_data();
-    pp.hitung_luas();
-    pp.output_data();
 };
